a4813d3a23cc35688a0f33e81b26dae82670b36b
Chen Yang <yangc5@mail.uc.edu>
2016-03-07 20:19:23 +0000

    Done.

--- js/jukebox.js
+++ js/jukebox.js
@@ -13,42 +13,32 @@ $(document).ready(function(){
   var theArtist = artists[Math.floor(Math.random()) * artists.length];
   var songToPlay = jukeboxSongs[theArtist];
 
-  function randomNumber() {
-    return Math.floor(Math.random());
-  }
-
   function menu(){
-    $('#menu').on('click', menuOptions);
-  }
-
-   function menuOptions() {
-    $('#menuOptions').html("You can play a song, pause a song, or view all the songs");
-  }
-
-  function nowPlaying(){
-    $('#songPlaying').html("now playing " + songToPlay + " by " + theArtist);
-    return "now playing " + songToPlay + " by " + theArtist;
+    $('#menu').on('click', function(){
+       $('#menuOptions').html("You can play a song, pause a song, or view all the songs");
+    });
   }
 
   function play(){
-    $('#play').on('click', nowPlaying)
-  }
-
-  function nowPausing(){
-    $('#songPaused').html(songToPlay + " is paused");
+    $('#play').on('click', function(){
+       $('#songPlaying').html("now playing " + songToPlay + " by " + theArtist);
+       return "now playing " + songToPlay + " by " + theArtist;
+    })
   }
 
   function pause(){
-    $('#pause').on('click', nowPausing);
+    $('#pause').on('click', function(){
+        $('#songPaused').html(songToPlay + " is paused");
+    });
   }
-   function showSongs(){
-    var songs = "";
-    for (var key in jukeboxSongs) {
-    songs += jukeboxSongs[key] + " by " + key +  " is available to play. "
-  }
-    $('#showSongs').html(songs);
-  }
+
   function show(){
-    $('#show').on('click', showSongs);
+    $('#show').on('click', function(){
+        var songs = "";
+        for (var key in jukeboxSongs) {
+        songs += jukeboxSongs[key] + " by " + key +  " is available to play. "
+      }
+        $('#showSongs').html(songs);
+    });
   }
 
